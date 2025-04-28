d5c2a44d2813871465f011acbdc4b24b1e82bac4
BB <brunobrouckaert@gmail.com>
2025-04-28 12:02:50 +0200

    Added code

--- logger.py
+++ logger.py
@@ -3,27 +3,17 @@ import time
 
 from config.config import CONSOLE_LOG_LEVEL
 
-class CustomFormatter(logging.Formatter):
-    """Custom formatter to format time with milliseconds correctly."""
-    def formatTime(self, record, datefmt=None):
-        ct = self.converter(record.created)
-        if datefmt:
-            s = self.formatter_time = self.formatter_time = time.strftime(datefmt, ct)
-        else:
-            t = time.strftime("%Y-%m-%d %H:%M:%S", ct)
-            s = f"{t}.{int(record.msecs):03d}"
-        return s
 
 def get_timestamp_logger():
-    logger = logging.getLogger("console")
-    logger.setLevel(CONSOLE_LOG_LEVEL)
+    new_logger = logging.getLogger("console")
+    new_logger.setLevel(CONSOLE_LOG_LEVEL)
 
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
 
     # Create a formatter with a human-readable timestamp
-    formatter = CustomFormatter(
-        fmt="%(asctime)s - %(levelname)s - %(message)s",
+    formatter = logging.Formatter(
+        fmt="%(asctime)s.%(msecs)03d - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S"
     )
 
@@ -31,8 +21,8 @@ def get_timestamp_logger():
     console_handler.setFormatter(formatter)
 
     # Attach the handler to the logger
-    logger.addHandler(console_handler)
-    return logger
+    new_logger.addHandler(console_handler)
+    return new_logger
 
 
 logger = get_timestamp_logger()
