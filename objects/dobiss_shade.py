22e2ef8525a70cf0ed0a3b3650dd0af54cf4722f
BB <brunobrouckaert@gmail.com>
2025-05-19 11:03:26 +0200

    Add cover position

--- objects/dobiss_shade.py
+++ objects/dobiss_shade.py
@@ -1,5 +1,8 @@
+import time
+
 from config.constants import SHADE, SHADE_STATE_OPEN, SHADE_COMMAND_CLOSE, SHADE_STATE_OPENING, SHADE_COMMAND_OPEN, \
-    SHADE_COMMAND_STOP, SHADE_STATE_CLOSED, SHADE_STATE_STOPPED
+    SHADE_COMMAND_STOP, SHADE_STATE_CLOSED, SHADE_STATE_STOPPED, SHADE_STATE_CLOSING, SHADE_COMMAND_TOGGLE_OPEN, \
+    SHADE_COMMAND_TOGGLE_CLOSE
 from mqtt.mqtt_worker import MqttWorker
 from objects.dobiss_entity import DobissEntity
 from objects.dobiss_relay import DobissRelay
@@ -12,12 +15,46 @@ class DobissShade(DobissEntity):
         self.status = SHADE_STATE_OPEN
         self.relay_up = relay_up
         self.relay_down = relay_down
+        self._position = 0
+        self._last_calculation_time = time.time()   # Calculation needs to be run before every move command to have a consistent position.
+        self.speed = 10  # % position change per second
+
+    @property
+    def position(self):
+        self.update_position()
+        return self._position
+
+    def update_position(self):
+        if self.status == SHADE_STATE_CLOSING:
+            self._position = int(self._position - (self.speed * (self._last_calculation_time - time.time())))
+            if self._position <= 0:
+                self._position = 0
+                self.status = SHADE_STATE_CLOSED
+                self.relay_down.set_status(0)
+                self.relay_down.set_status(0)
+                self._last_calculation_time = time.time()
+            self.report_state_to_mqtt()
+        elif self.status == SHADE_STATE_OPENING:
+            self._position = int(self._position + (self.speed * (self._last_calculation_time - time.time())))
+            if self._position >= 100:
+                self._position = 100
+                self.status = SHADE_STATE_OPEN
+                self.relay_down.set_status(0)
+                self.relay_down.set_status(0)
+                self._last_calculation_time = time.time()
+            self.report_state_to_mqtt()
 
     def get_mqtt_state_topic(self):
         return f"homeassistant/cover/{self.name}/state"
 
+    def get_mqtt_position_topic(self):
+        return f"homeassistant/light/{self.name}/position"
+
     def get_mqtt_command_topic(self):
         return f"homeassistant/cover/{self.name}/set"
+
+    def get_mqtt_set_position_topic(self):
+        return f"homeassistant/light/{self.name}/set_position"
 
     def get_discover_topic(self):
         return f"homeassistant/cover/{self.name}/config"
@@ -33,16 +70,42 @@ class DobissShade(DobissEntity):
 
     def set_status(self, command, brightness=100):
         if command == SHADE_COMMAND_OPEN:
-            self.relay_up.set_status(1)
-            self.status = SHADE_STATE_OPEN
+            self.open()
         elif command == SHADE_COMMAND_CLOSE:
-            self.relay_down.set_status(1)
-            self.status = SHADE_STATE_CLOSED
+            self.close()
         elif command == SHADE_COMMAND_STOP:
-            self.relay_down.set_status(0)
-            self.relay_down.set_status(0)
-            self.status = SHADE_STATE_STOPPED
+            self.stop()
+        elif command == SHADE_COMMAND_TOGGLE_OPEN:
+            if self.status == SHADE_STATE_OPENING:
+                self.stop()
+            else:
+                self.open()
+        elif command == SHADE_COMMAND_TOGGLE_CLOSE:
+            if self.status == SHADE_STATE_CLOSING:
+                self.stop()
+            else:
+                self.close()
         else:
             raise Exception(f"Unknown command: {command}")
 
+        self._last_calculation_time = time.time()
         self.report_state_to_mqtt()
+
+    def open(self):
+        self.update_position()
+        self.relay_up.set_status(1)
+        self.status = SHADE_STATE_OPENING
+
+    def close(self):
+        self.update_position()
+        self.relay_down.set_status(1)
+        self.status = SHADE_STATE_CLOSING
+
+    def stop(self):
+        self.update_position()
+        self.relay_down.set_status(0)
+        self.relay_down.set_status(0)
+        self.status = SHADE_STATE_STOPPED
+
+
+
